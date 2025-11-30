0447634778291b9684ca4f19a20890b4f2b06c9d
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-11-30 23:38:25 +0530

    We write controller for redirect for login user

---
+++ src/main/java/com/mini_zira/controllers/redirection/LoginRedirection.java
@@ -0,0 +1,22 @@
+package com.mini_zira.controllers.redirection;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "longin-redirect", value = "/auth/login")
+public class LoginRedirection extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp);
+
+        RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/views/pages/login.jsp");
+        rd.forward(req,resp);
+    }
+}
