bbdea33191707a556bb9e72f5bdaa9f364ecaf5a
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-12-01 22:37:40 +0530

    we write controller for login the user

---
+++ src/main/java/com/mini_zira/controllers/auth/Login.java
@@ -0,0 +1,42 @@
+package com.mini_zira.controllers.auth;
+
+import com.mini_zira.service.Auth.AuthServiceImpl;
+import com.mini_zira.service.users.UserService;
+import com.mini_zira.service.users.UserServiceImpl;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class Login extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doPost(req, resp);
+
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+        String role = req.getParameter("role");
+
+        RequestDispatcher rd = req.getRequestDispatcher("WEB-INF/views/pages/login.jsp");
+
+        if(email.isEmpty() || password.isEmpty() || role.isEmpty()){
+            req.setAttribute("error","Please fill all the fields");
+            rd.include(req, resp);
+            return;
+        }
+
+        AuthServiceImpl auth = new AuthServiceImpl();
+        boolean res = auth.loginUser(email, password);
+
+        if(res == true){
+            req.getSession().setAttribute("login", "login-user");
+            resp.sendRedirect((req.getContextPath()+"/home"));
+        }else{
+            req.setAttribute("error","Get Error during login");
+            rd.include(req, resp);
+        }
+    }
+}
