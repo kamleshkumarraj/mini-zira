0c6401cf5cf9110db6f9f37c9b2d03678da8b7a5
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-12-02 11:21:25 +0530

    we write authentication filter for profile page

---
+++ src/main/java/com/mini_zira/filters/auth/IsLoggedIn.java
@@ -0,0 +1,43 @@
+package com.mini_zira.filters.auth;
+
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.mini_zira.entities.Users;
+import com.mini_zira.services.profile.ProfileServiceImpl;
+import com.mini_zira.utility.Auth.JWTUtils;
+import com.mini_zira.utility.Cookie.CookieUtils;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {
+        "/app/profile"
+})
+public class IsLoggedIn implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        // first we get token.
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+        String token = CookieUtils.getCookie(req, "token");
+
+        if(token == null || token.isEmpty()){
+            req.getSession().setAttribute("message", "please login to access this resource !");
+            resp.sendRedirect(req.getContextPath() + "/auth/login");
+            return;
+        }
+
+        DecodedJWT jwt = JWTUtils.verifyToken(token);
+        if(jwt == null){
+            req.getSession().setAttribute("message", "please login to access this resource !");
+            resp.sendRedirect(req.getContextPath() + "/auth/login");
+            return;
+        }
+
+        request.setAttribute("email", jwt.getClaim("email").asString());
+        chain.doFilter(request, response);
+
+    }
+}
